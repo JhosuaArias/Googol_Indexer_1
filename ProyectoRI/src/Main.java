3bc4a6be0159d70805a29a01a38c36beb621e655
JhosuaArias <jhosuarqui@hotmail.com>
2018-09-05 19:27:42 -0600

    Initial Commit

---
+++ ProyectoRI/src/Main.java
@@ -0,0 +1,14 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+            Document doc = Jsoup.parse(html);
+            String body = doc.body().text();
+            System.out.println(body);
+    }
+}
