4654cd122c89336b3ac0f29923eb5e4730693ea3
Elias Calderon <eliascr98@outlook.com>
2018-09-11 08:32:58 -0600

    Indexer y FileHandler añadidos

---
+++ ProyectoRI/src/FileHandler.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class FileHandler {
+
+    private static StringBuilder sb;
+    private static List<String> stringArray;
+
+    public static String readFileSB(String path) {
+        sb = new StringBuilder();
+        FileHandler.abstractReadFile(path, FileHandler::appendSB);
+
+        return sb.toString();
+    }
+
+    public static List<String> readFileStringArray(String path) {
+        stringArray = new ArrayList<>();
+        FileHandler.abstractReadFile(path, FileHandler::appendStringArray);
+
+        return stringArray;
+    }
+
+    private static void abstractReadFile(String path, Consumer<? super String> appendFunction) {
+
+        try (Stream<String> stream = Files.lines(Paths.get(path))) {
+
+            stream.forEach(appendFunction);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void appendSB(String line) {
+        sb.append(line).append(" ");
+    }
+
+    private static void appendStringArray(String line) {
+        stringArray.add(line);
+    }
+
+}
--- ProyectoRI/src/Main.java
+++ ProyectoRI/src/Main.java
@@ -6,9 +6,12 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-            String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
-            Document doc = Jsoup.parse(html);
-            String body = doc.body().text();
-            System.out.println(body);
+        String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+        Document doc = Jsoup.parse(html);
+        String body = doc.body().text();
+        System.out.println(body);
+
+        Indexer i = new Indexer();
+        i.indexFiles();
     }
 }
