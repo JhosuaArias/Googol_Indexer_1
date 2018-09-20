251db71a897fea4c7d1691a6a9eed0e6fe05719d
JhosuaArias <jhosuarqui@hotmail.com>
2018-09-19 20:08:08 -0600

    TermData update.

--- src/main/java/cr/ac/ucr/ecci/TermData.java
+++ src/main/java/cr/ac/ucr/ecci/TermData.java
@@ -1,4 +1,57 @@
 package cr.ac.ucr.ecci;
 
+import java.util.ArrayList;
+
 public class TermData {
+    private String term;
+    private ArrayList<String> documents;
+    private double inverseFrequency;
+    private double frecuency;
+    private int timesAppeared;
+
+    public TermData(String term) {
+        this.setTerm(term);
+        this.setDocuments(new ArrayList<>());
+        this.setInverseFrequency(0);
+        this.setTimesAppeared(1);
+    }
+
+
+
+    public void computeInversedFrecuency(){
+
+    }
+
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public ArrayList<String> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(ArrayList<String> documents) {
+        this.documents = documents;
+    }
+
+    public double getInverseFrequency() {
+        return inverseFrequency;
+    }
+
+    public void setInverseFrequency(double inverseFrequency) {
+        this.inverseFrequency = inverseFrequency;
+    }
+
+    public int getTimesAppeared() {
+        return timesAppeared;
+    }
+
+    public void setTimesAppeared(int timesAppeared) {
+        this.timesAppeared = timesAppeared;
+    }
 }
